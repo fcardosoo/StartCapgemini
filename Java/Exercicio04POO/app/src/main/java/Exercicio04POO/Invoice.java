385d5e993f8f2694e00d2eb016c895627167a53f
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2023-03-24 17:03:01 -0300

    Update Invoice.java

--- Java/Exercicio04POO/app/src/main/java/Exercicio04POO/Invoice.java
+++ Java/Exercicio04POO/app/src/main/java/Exercicio04POO/Invoice.java
@@ -18,10 +18,51 @@ public class Invoice {
     public Invoice(int codigoItem, String descricaoItem, int quantidadeComprada, float precoUnit) {
         this.codigoItem = codigoItem;
         this.descricaoItem = descricaoItem;
+        
+        if (quantidadeComprada < 0){
+            this.quantidadeComprada = 0;
+        } else{
+            this.quantidadeComprada = quantidadeComprada;
+        }
+        
+        if(precoUnit < 0){
+            this.precoUnit = 0;
+        } else {
+            this.precoUnit = precoUnit;
+        }        
+    }
+
+    public int getCodigoItem(){
+        return codigoItem;
+    }
+    
+    public void setCodigoItem(int codigoItem){
+        this.codigoItem = codigoItem;
+    }
+
+    public String getDescricaoItem() {
+        return descricaoItem;
+    }
+
+    public void setDescricaoItem(String descricaoItem) {
+        this.descricaoItem = descricaoItem;
+    }
+
+    public int getQuantidadeComprada() {
+        return quantidadeComprada;
+    }
+
+    public void setQuantidadeComprada(int quantidadeComprada) {
         this.quantidadeComprada = quantidadeComprada;
+    }
+
+    public float getPrecoUnit() {
+        return precoUnit;
+    }
+
+    public void setPrecoUnit(float precoUnit) {
         this.precoUnit = precoUnit;
     }
- 
     
     
 }
