5635066600bf9e2d163095d62b28ae37158604cf
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2023-03-24 13:17:25 -0300

    exercice 04 poo

---
+++ Java/Exercicio04POO/app/src/main/java/Exercicio04POO/App.java
@@ -0,0 +1,27 @@
+/*
+ Criar uma classe chamada 'Invoice' que possa ser utilizado por uma loja de
+suprimentos de informática para representar uma fatura de um item vendido na
+loja. Uma fatura deve incluir as seguintes informações como atributos:
+
+a. o número do item faturado;
+b. a descrição do item;
+c. a quantidade comprada do item;
+d. o preço unitário do item;
+
+Sua classe deve ter um construtor que inicialize os quatro atributos. Se a quan-
+tidade não for positiva, ela deve ser configurada como 0. Se o preço por item
+for positivo ele deve ser configurado como 0.0. Forneça um método set e um méto-
+do get para cada variável de instância.
+ */
+package Exercicio04POO;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        
+        
+    }
+}
---
+++ Java/Exercicio04POO/app/src/main/java/Exercicio04POO/Invoice.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exercicio04POO;
+
+/**
+ *
+ * @author d311r
+ */
+public class Invoice {
+    
+    private int codigoItem;
+    private String descricaoItem;
+    private int quantidadeComprada;
+    private float precoUnit;
+
+    public Invoice(int codigoItem, String descricaoItem, int quantidadeComprada, float precoUnit) {
+        this.codigoItem = codigoItem;
+        this.descricaoItem = descricaoItem;
+        this.quantidadeComprada = quantidadeComprada;
+        this.precoUnit = precoUnit;
+    }
+ 
+    
+    
+}
