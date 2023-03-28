c5871f2f21ef571c1f9e65afafb194b4e19cf5f2
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2023-03-28 15:59:23 -0300

    database conection

--- Java/TodoApp/app/src/main/java/util/ConnectionFactory.java
+++ Java/TodoApp/app/src/main/java/util/ConnectionFactory.java
@@ -10,4 +10,9 @@ package util;
  */
 public class ConnectionFactory {
     
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/todoApp";
+    public static final String USER = "root";
+    public static final String PASS = "";
+    
 }
