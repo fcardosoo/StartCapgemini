f2109819bf458d4e3895c8077f9e6c2929dbd50d
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2023-03-28 16:56:44 -0300

    Update ConnectionFactory.java

--- Java/TodoApp/app/src/main/java/util/ConnectionFactory.java
+++ Java/TodoApp/app/src/main/java/util/ConnectionFactory.java
@@ -4,6 +4,10 @@
  */
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 /**
  *
  * @author d311r
@@ -16,5 +20,22 @@ public class ConnectionFactory {
     public static final String PASS = "";
     
     
+    public static Connection getConnection(){
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception ex) {
+            throw new RuntimeException ("Erro na conexão com o banco de dados", ex);
+        }
+    }
     
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao fechar a conexão com o banco de dados", ex);
+        }
+    }
 }
