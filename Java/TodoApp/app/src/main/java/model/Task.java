5e67992e417057229bff2c7893502e56a2d64345
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2023-03-30 10:30:38 -0300

    Update Task.java

--- Java/TodoApp/app/src/main/java/model/Task.java
+++ Java/TodoApp/app/src/main/java/model/Task.java
@@ -32,7 +32,11 @@ public class Task {
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
     }
-
+    
+    public Task(){
+        this.createdAt = new Date();
+    }
+    
     public int getId() {
         return id;
     }
