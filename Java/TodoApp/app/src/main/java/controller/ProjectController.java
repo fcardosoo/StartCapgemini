c6b484e091887be31bf074b34a35488328cb8eaa
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2023-03-31 16:31:38 -0300

    Update ProjectController.java

--- Java/TodoApp/app/src/main/java/controller/ProjectController.java
+++ Java/TodoApp/app/src/main/java/controller/ProjectController.java
@@ -6,8 +6,11 @@ package controller;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import model.Project;
 import util.ConnectionFactory;
 
@@ -72,6 +75,65 @@ public class ProjectController {
         } catch (SQLException ex) {
             throw new RuntimeException("Erro ao atualizar o projeto", ex);
         } finally {
+            ConnectionFactory.closeConnection(connection, statement);
+        }
+    }
+    
+    public List<Project> getAll() {
+        
+        String sql = "SELECT * FROM projects";
+        
+        List<Project> projects = new ArrayList<>();
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+        //Classe que vai recuperar os dados do banco de dados
+        ResultSet resultSet = null;
+        
+        try {
+            connection = ConnectionFactory.getConnection();
+            statement = connection.prepareStatement(sql);
+            
+            resultSet = statement.executeQuery();
+            
+            //Enquanto existir dados no banco de dados, faça
+            while (resultSet.next()) {
+                
+                Project project = new Project();
+                
+                project.setId(resultSet.getInt("id"));
+                project.setName(resultSet.getString("name"));
+                project.setDescription(resultSet.getString("description"));
+                project.setCreatedAt(resultSet.getDate("createdAt"));
+                project.setCreatedAt(resultSet.getDate("updatedAt"));
+                
+                //Adiciono o contato recuperado, a lista de contatos
+                projects.add(project);
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException ("Erro ao buscar os projetos", ex);
+        } finally {
+            ConnectionFactory.closeConnection(connection, statement, resultSet);
+        }
+        return projects;
+    }
+    
+    public void removeId(int id) {
+        
+        String sql = "DELETE FROM projects WHERE id = ?";
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+        try {
+            connection = ConnectionFactory.getConnection();
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, id);
+            statement.execute();
+        } catch(SQLException ex){
+            throw new RuntimeException("Erro ao deletar o projeto", ex);
+        }finally {
             ConnectionFactory.closeConnection(connection, statement);
         }
     }
