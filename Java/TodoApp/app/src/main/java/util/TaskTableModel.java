df62c777c93b51886d0b3c1a5cf8ef5fe0da95af
fcardosoo <63169279+fcardosoo@users.noreply.github.com>
2023-04-12 17:18:40 -0300

    Update TaskTableModel.java

--- Java/TodoApp/app/src/main/java/util/TaskTableModel.java
+++ Java/TodoApp/app/src/main/java/util/TaskTableModel.java
@@ -4,14 +4,20 @@
  */
 package util;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.table.AbstractTableModel;
+import model.Task;
 
 /**
  *
  * @author d311r
  */
 public class TaskTableModel extends AbstractTableModel{
-
+    
+    String[] columns = {"Nome", "Descrição", "Prazo", "Tarefa Concluída", "Editar", "Excluir"};
+    List<Task> tasks = new ArrayList();
+    
     @Override
     public int getRowCount() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
